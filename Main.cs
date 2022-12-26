2aeb2d4530134e367add201fe7a0e31213463192
Luka Grenon <lukagrenon@icloud.com>
2022-12-26 12:18:21 -0500

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,35 @@
+﻿using MelonLoader;
+using UnityEngine;
+using HarmonyLib;
+
+namespace SimpleACTK
+{
+    public static class BuildInfo
+    {
+        public const string Name = "SimpleACTK"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "Bypasses ACTK Dll Force Crash."; // Description for the Mod.  (Set as null if none)
+        public const string Author = "Mathie"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class SimpleACTK : MelonMod
+    {
+        public override void OnApplicationStart() // Runs after Game Initialization.
+        {
+            MelonLogger.Msg("Patching application quit!");
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Application), "Quit", new Type[] { })]
+        class ApplicationQuit2
+        {
+            [HarmonyLib.HarmonyPrefix]
+            public unsafe static bool Prefix()
+            {
+                MelonLogger.Msg("Application Quit Called!");
+                return false;
+            }
+        }
+    }
+}
